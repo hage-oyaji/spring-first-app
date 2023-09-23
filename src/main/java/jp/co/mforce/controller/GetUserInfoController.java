e2307dd0ce490e300cd2c62760a9f064dbc928e1
takuto_kitabatake <takuto_kitabatake@mforce.co.jp>
2023-09-24 08:58:24 +0900

    jsonparseの追加

--- src/main/java/jp/co/mforce/controller/GetUserInfoController.java
+++ src/main/java/jp/co/mforce/controller/GetUserInfoController.java
@@ -5,6 +5,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+
 import jp.co.mforce.resource.GetUserInfoResource;
 import jp.co.mforce.service.GetUserInfoService;
 
@@ -17,7 +19,7 @@ public class GetUserInfoController {
     @GetMapping("/userInfo")
     public Object getUserInfo(
     		final GetUserInfoResource request,
-    		final BindingResult bindingResult) {
+    		final BindingResult bindingResult) throws JsonProcessingException {
 
 		return service.createUserInfo(
 					request,
--- src/main/java/jp/co/mforce/service/GetUserInfoService.java
+++ src/main/java/jp/co/mforce/service/GetUserInfoService.java
@@ -3,6 +3,9 @@ package jp.co.mforce.service;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.BindingResult;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import jp.co.mforce.resource.GetUserInfoResource;
 
 @Service
@@ -10,13 +13,16 @@ public class GetUserInfoService {
 
 	public Object createUserInfo(
 			final GetUserInfoResource request,
-			final BindingResult bindingResult) {
+			final BindingResult bindingResult) throws JsonProcessingException {
 
 		GetUserInfoResource	getUserInfoResource = new GetUserInfoResource();
 		getUserInfoResource.setUserName(request.getUserName());
 		getUserInfoResource.setMailAddress(request.getMailAddress());
 
-		return getUserInfoResource;
+        ObjectMapper mapper = new ObjectMapper();
+        String json = mapper.writeValueAsString(getUserInfoResource);
+
+		return (Object)json;
 	}
 
 }
